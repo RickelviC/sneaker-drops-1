c50987a5af70a31d6c23b9e29dd7f58d3ca2c03c
RickelviC <Cruzrickelvi@gmail.com>
2026-06-08 14:31:08 -0400

    loading sneakers

--- src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
@@ -1,4 +1,22 @@
 package com.pluralsight.sneakerdrops;
 
-public class StartupRunner {
+import com.pluralsight.sneakerdrops.service.DropService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    private final DropService runner;
+
+    @Autowired
+    public StartupRunner(DropService runner) {
+        this.runner = runner;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(runner.getStatus());
+    }
 }
